bdec314eda57eb61ef28b634e3ce73fe5d0dac36
joel-you <ruew12@gmail.com>
2024-03-19 22:02:20 +0900

    feat: Domain 추가
    - Order
    - OrderItem

---
+++ src/main/java/domain/OrderItem.java
@@ -0,0 +1,17 @@
+package domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "order_items")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String product;
+
+    private Integer quantity;
+
+}
